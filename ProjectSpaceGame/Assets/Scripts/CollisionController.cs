620929e5d6a024999ab076c1b7c4c0e68ed180a4
TomaszKrosnicki <krosnickitomek@gmail.com>
2022-01-29 02:59:00 +0100

    Explosion sound bugfix

--- ProjectSpaceGame/Assets/Scripts/CollisionController.cs
+++ ProjectSpaceGame/Assets/Scripts/CollisionController.cs
@@ -14,6 +14,7 @@ public class CollisionController : MonoBehaviour
     [SerializeField] ParticleSystem explosionParticle;
     [SerializeField] ParticleSystem[] enginesParticles;
     MeshRenderer mr;
+    MeshCollider mc;
     void Start() 
     {
         PlayerPrefs.SetInt("score",0);
@@ -21,6 +22,7 @@ public class CollisionController : MonoBehaviour
         PlayerPrefs.SetInt("distance",0);
         audiosource = GetComponent<AudioSource>();
         mr = GetComponent<MeshRenderer>();
+        mc = GetComponent<MeshCollider>();
     }
 
     void Update() 
@@ -33,12 +35,12 @@ public class CollisionController : MonoBehaviour
         if (other.gameObject.tag == "Asteroid" && !PlayerState.isDead)
         {
             PlayerState.isDead = true;
-            if(!audiosource.isPlaying)
             audiosource.PlayOneShot(explosion);
             explosionParticle.Play();
             enginesParticles[0].Stop();
             enginesParticles[1].Stop();
             mr.enabled = false;
+            mc.enabled = false;
             SpawnFracturedObj();
         }
         else if (other.gameObject.tag == "Star")
